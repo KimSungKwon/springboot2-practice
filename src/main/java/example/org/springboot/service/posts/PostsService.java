bbf3d3e02529bf44e11cd045a53804f5a73adad8
KimSungKwon <sungkwonn@naver.com>
2021-01-29 23:18:19 +0900

    Add Posts List at "/"

--- src/main/java/example/org/springboot/service/posts/PostsService.java
+++ src/main/java/example/org/springboot/service/posts/PostsService.java
@@ -6,12 +6,16 @@ package example.org.springboot.service.posts;
 
 import example.org.springboot.domain.posts.Posts;
 import example.org.springboot.domain.posts.PostsRepository;
+import example.org.springboot.web.dto.PostsListResponseDto;
 import example.org.springboot.web.dto.PostsResponseDto;
 import example.org.springboot.web.dto.PostsSaveRequestDto;
 import example.org.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +24,7 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
+
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
@@ -37,5 +42,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 조회 기능만. (속도 개선)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)     // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/example/org/springboot/web/IndexController.java
+++ src/main/java/example/org/springboot/web/IndexController.java
@@ -1,18 +1,25 @@
 package example.org.springboot.web;
 
+import example.org.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {  // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장가능. postsService.fAD()로 가져온 결과를
+        model.addAttribute("posts", postsService.findAllDesc());    // posts로 index.mustache에 전달함.
         return "index"; // src/main/resources/templates/index.mustache 로 전환되어 View Resolver가 처리함.
     }
 
     @GetMapping("/posts/save")
     public  String savePosts() {
+
         return "posts-save";    // call posts-save.mustache
     }
 }
